ef21aac011c269209289d5332c3ec2037a782d90
Zane Gamin <zane.gamin@gmail.com>
2024-04-09 15:55:24 -0400

    Took out some strange >>>>

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -38,11 +38,8 @@
       </router-link>
     </nav>
     <h2>Recent Decks</h2>
-    <<<<<<< HEAD
+
     <UserDecks />
-    =======
-    <UserDecks />
-    >>>>>>> main
   </div>
 </template>
 
@@ -80,26 +77,25 @@ export default {
           console.error('Error fetching data:', error);
         });
        },
-<<<<<<< HEAD
-<<<<<<< HEAD
+
        getNextDeckId(){
           return this.nextDeckId++;
        },
        createDeck(){
           this.newDeck.id =
+       },
 
-=======
        goToCreate(){
         this.$router.push("/create");
->>>>>>> 38288b2 (create deck method)
-       }
 
-=======
+       },
+
+
        goToCreate(){
         this.$router.push("/create");
-       }
+       },
 
->>>>>>> main
+
   },
 }
 </script>
