c2cb7f395f0cb524726cc98316d96b25cbebdcce
Zane Gamin <zane.gamin@gmail.com>
2024-04-09 14:16:20 -0400

    merge

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, flashcard, deck CASCADE;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -10,5 +10,23 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE flaschard (
+	card_id SERIAL,
+	deck_id numeric,
+	question varchar(500) NOT NULL,
+	answer varchar(2000) NOT NULL,
+	CONSTRAINT PK_flashcard PRIMARY KEY (card_id),
+	CONSTRAINT FK_flashcard_deck FOREIGN KEY (deck_id) references (deck_id)
+);
+
+CREATE TABLE deck (
+	deck_id SERIAL,
+ 	name varchar(50) NOT NULL,
+	high_score numeric(100) NOT NULL DEFAULT '0',
+	color numeric(10) NOT NULL,
+	creator_id numeric(50) NOT NULL,
+ 	CONSTRAINT PK_deck PRIMARY KEY (deck_id),
+	CONSTRAINT FK_deck_users FOREIGN KEY (creator_id) references (user_id)
+);
 
 COMMIT TRANSACTION;
--- vue/src/views/LogoutView.vue
+++ vue/src/views/LogoutView.vue
@@ -1,9 +1,14 @@
 <template>
+  <Header/>
   <h1>Logout</h1>
 </template>
 
 <script>
+import Header from "../components/Header.vue"
 export default {
+  components: {
+    Header
+  },
   created() {
     this.$store.commit("LOGOUT");
     this.$router.push("/login");
