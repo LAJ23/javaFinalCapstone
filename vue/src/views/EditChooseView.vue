d2363abee21a2fc70935a328d691f6643e052ba1
Zane Gamin <zane.gamin@gmail.com>
2024-04-15 15:37:25 -0400

    Adding Cards

--- vue/src/views/EditChooseView.vue
+++ vue/src/views/EditChooseView.vue
@@ -2,34 +2,20 @@
 
     <h2>Choose a deck to Edit:</h2>
     <UserDecks route-name="editDeck" />
-
-  
-          
-   
   </template>
-  
   <script>
-
   import UserDecks from '../components/UserDecks.vue';
-
-  
-  
   export default {
     components: {
-
       UserDecks,
-      
     }
   };
   </script>
-  
   <style scoped>
   * {
     font-family: 'Writing';
     padding-left: 10vw;
     padding-right: 10vw;
-  
-  
   }
   
   h2 {
